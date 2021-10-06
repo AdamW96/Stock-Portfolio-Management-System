2ae0e9014663f50eb530e31db4ecea742400ceed
ruiwang14 <sprucewang@163.com>
2021-10-06 20:45:34 +0800

    update deploy scripts

--- backend/start.sh
+++ backend/start.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
-rm -rf /usr/src/app
-cp -r . /usr/src/app
+# /usr/src/app is share_data dir
+rm /usr/src/app/9900-project.log
+cp 9900-project-test.jar /usr/src/app/9900-project-test.jar
+cp start.sh /usr/src/app/start.sh
+
+if [ -f "./9900-test-db.sqlite" ];then
+  echo "use shared_data db"
+  else
+  echo "use initialized db"
+  cp 9900-test-db.sqlite /usr/src/app/9900-test-db.sqlite
+fi
 
 cd /usr/src/app
 
 echo "\$pwd"
 pwd
 
-echo "\$java -jar 900-project-test.jar"
+echo "\$java -jar 9900-project-test.jar"
 java -jar 9900-project-test.jar

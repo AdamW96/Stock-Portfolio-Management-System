f1ad0103065cd38e676b7e590d872dafaed1531b
ruiwang14 <sprucewang@163.com>
2021-10-06 19:54:53 +0800

    update deploy scripts

--- backend/project_run.sh
+++ backend/project_run.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+echo "\$docker pull registry.cn-qingdao.aliyuncs.com/unsw/9090-project:latest"
+docker pull registry.cn-qingdao.aliyuncs.com/unsw/9090-project:latest
+
 echo "\$docker run -p 8081:8080 -v \"\$(pwd)/share_data\":/usr/src/app --name 9900-project registry.cn-qingdao.aliyuncs.com/unsw/9090-project:latest"
 docker run -d -p 8081:8080 -v "$(pwd)/share_data":/usr/src/app --name 9900-project registry.cn-qingdao.aliyuncs.com/unsw/9090-project:latest

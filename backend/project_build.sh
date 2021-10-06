97813ed14921e5d9dce92834ec102de2f9ca9487
ruiwang14 <sprucewang@163.com>
2021-10-06 20:45:34 +0800

    update deploy scripts

--- backend/project_build.sh
+++ backend/project_build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "\$mvn package"
-mvn package
+mvn package || { echo "error when build jar"; exit 1; }
 
 echo "\$docker build -t registry.cn-qingdao.aliyuncs.com/unsw/9090-project ."
 docker build -t registry.cn-qingdao.aliyuncs.com/unsw/9090-project .

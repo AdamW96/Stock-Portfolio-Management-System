1ba2d0cfc750c870877b5dfe91a5e4d7e8e734d6
feifeirun <feifeirun97@gmail.com>
2021-10-07 19:45:57 +1100

    day2

--- frontend/src/Components/Right.js
+++ frontend/src/Components/Right.js
@@ -1,10 +1,18 @@
-import { alpha, AppBar, Avatar, Badge, IconButton, InputBase, makeStyles, Menu, MenuItem, MenuList, Toolbar, Typography } from '@material-ui/core'
-import { Chat, Mail, Notifications, Search } from '@material-ui/icons'
+import {  Card, Container, IconButton, InputBase, makeStyles, Menu, MenuItem, MenuList, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Toolbar, Typography } from '@material-ui/core'
+import { Add  } from '@material-ui/icons'
 import { useState } from 'react';
+import SearchIcon from '@material-ui/icons/Search';
+
 
 const useStyles = makeStyles(theme => ({
-  sad:{
+  container:{
     paddingTop:theme.spacing(12)
+  },
+  text:{
+    fontFamily: 'Bungee',
+    fontSize:theme.spacing(5),
+    color:'#FF954A',
+    marginLeft:theme.spacing(1),
   }
 }))
 
@@ -12,8 +20,10 @@ export default function Right() {
   const classes = useStyles();
   const [userMenu, setUserMenu] = useState(false);
   return (
-    <div className={classes.sad}>
-      rightbar
-    </div>
+    <Container className={classes.container}>
+      <Paper>
+        <h1 className={classes.text}>Your Rank</h1>
+      </Paper>
+    </Container>
   )
 }

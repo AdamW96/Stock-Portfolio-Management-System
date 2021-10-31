6f9ca3f4eca1861fdb6bb3963da853f65be59feb
adam <wangzhao960917@gmail.com>
2021-10-31 22:38:39 +1100

    fix sign in URL

--- frontend/src/services/auth-service.js
+++ frontend/src/services/auth-service.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
-const API_URL = "https://nodejs-9900-backend.herokuapp.com/api/user"; //backend API address
+const API_URL = "http://localhost:8080"; //backend API address
 
 class AuthService {
   signin(email, password) {
-    return axios.post(API_URL + "/signin", { email, password });
+    return axios.post(API_URL + "user/signIn", { email, password });
   }
   logout() {
     localStorage.removeItem("user");
   }
   register(firstName, lastName, userName, email, password) {
-    return axios.post(API_URL + "/register", {
+    return axios.post(API_URL + "user/register", {
       firstname: firstName,
       lastname: lastName,
       username: userName,

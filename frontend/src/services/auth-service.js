8de15627fbf6ebaf39b715919b065385e9995df4
adam <wangzhao960917@gmail.com>
2021-11-16 21:57:07 +1100

    add md5 in frontend

--- frontend/src/services/auth-service.js
+++ frontend/src/services/auth-service.js
@@ -1,23 +1,25 @@
 import axios from "axios";
 import BACKEND_URL from "./config";
-
+import md5 from "js-md5";
 const API_URL = BACKEND_URL; //backend API address
 
 class AuthService {
   signin(email, password) {
     axios.defaults.withCredentials = true
-    return axios.post(API_URL + "/user/signIn", { email, pwdMd5: password });
+    const pwdMd5 = md5(password)
+    return axios.post(API_URL + "/user/signIn", { email, pwdMd5});
   }
   logout() {
     localStorage.removeItem("user");
   }
   register(firstName, lastName, userName, email, password) {
+    const pwdMd5 = md5(password)
     return axios.post(API_URL + "/user/register", {
       firstName: firstName,
       lastName: lastName,
       userName: userName,
       email,
-      pwdMd5: password,
+      pwdMd5: pwdMd5,
     });
   }
   getCurrentUser() {
